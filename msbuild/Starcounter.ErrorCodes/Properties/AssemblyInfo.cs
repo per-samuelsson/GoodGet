72a6b8f1969accef0c9b89338a549979c3ed730d
Per Samuelsson <per@starcounter.com>
2013-12-06 11:19:11 +0100

    Bound Starcounter.ErrorCodes to the Starbits common assembly metadata.

---
+++ msbuild/Starcounter.ErrorCodes/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Starcounter.ErrorCodes")]
+[assembly: AssemblyDescription("Expose the Starcounter error codes to .NET consumers.")]
+
+[assembly: Guid("388bcdad-55de-4838-bf89-094f05361de1")]
