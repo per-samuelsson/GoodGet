0859426f8ed545ef6b476ad769a9e343de869c0f
Per Samuelsson <per@starcounter.com>
2013-12-13 12:25:18 +0100

    Began a port of GoodGet to support it as a stand-alone exectuable with the simplest possible usage: "goodget packageId".

---
+++ msbuild/GoodGet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodGet {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length == 0) {
+                Console.WriteLine("Usage: goodget packageId [options]");
+            }
+        }
+    }
+}
