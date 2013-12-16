15d63de31eb22bb4a0bf01766f26e7f5f6d11ac9
Per Samuelsson <per@starcounter.com>
2013-12-16 20:47:36 +0100

    Added GoodGetModule to the GoodGet CLI/library, representing the GoodGet module itself, exposing the pattern we use for lightweight dependency injection.

---
+++ msbuild/GoodGet/GoodGetModule.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace Modules {
+    /// <summary>
+    /// Represents the GoodGet module itself, and the pattern we
+    /// use to support lightweight depedency injection.
+    /// </summary>
+    public static class GoodGetModule {
+
+        /// <summary>
+        /// Contains all dependency injections into this module
+        /// </summary>
+        public static class Injections {
+            /// <summary>
+            /// Sample dependency.
+            /// </summary>
+            public static IDisposable Sample = null;
+        }
+    }
+}
