4382b58f746ceeca5595d8d60a64dfd8d10a4bb4
Per Samuelsson <per@starcounter.com>
2013-12-16 21:26:45 +0100

    Added FeedPackages class, representing a set of packages to be synchronized with a given feed.

---
+++ msbuild/GoodGet/FeedPackages.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace GoodGet {
+
+    /// <summary>
+    /// Set of packages specified for certain <see cref="Feed"/>.
+    /// </summary>
+    internal sealed class FeedPackages {
+        /// <summary>
+        /// The feed the specified set of packages are to be
+        /// synchronized with.
+        /// </summary>
+        public readonly Feed Feed;
+        
+        /// <summary>
+        /// The set of packages to be synchronized from the
+        /// given feed.
+        /// </summary>
+        public List<string> Packages = new List<string>();
+
+        /// <summary>
+        /// Initialize a <see cref="FeedPackages"/> instance.
+        /// </summary>
+        /// <param name="feed">The <see cref="Feed"/> to specify
+        /// packages for.</param>
+        public FeedPackages(Feed feed) {
+            if (feed == null) {
+                throw new ArgumentNullException("feed");
+            }
+            Feed = feed;
+        }
+    }
+}
