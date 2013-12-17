6bdec53d72c9510f951c81ef251116ed86ff6564
Per Samuelsson <per@starcounter.com>
2013-12-17 14:05:01 +0100

    Added the InstallerContext class to GoodGet, governing the installation of packages via a given installer (on a certain feed).

---
+++ msbuild/GoodGet/InstallerContext.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace GoodGet {
+    /// <summary>
+    /// Container for a given <see cref="IInstaller"/>, feeding the installer
+    /// with the neccessary input and controlling the order in which packages
+    /// are installed.
+    /// </summary>
+    internal sealed class InstallerContext {
+        readonly PackagesFolder folder;
+        readonly string[] packages;
+        readonly IInstaller installer;
+        readonly IGot got;
+
+        public InstallerContext(PackagesFolder folder, string[] packages, IInstaller installer, IGot got) {
+            this.folder = folder;
+            this.packages = packages;
+            this.installer = installer;
+            this.got = got;
+        }
+
+        public void Install() {
+            Console.WriteLine("Installing {0} packages from {1} to {2}", packages.Length, installer.Feed.DisplayName, folder.Path);
+
+            var installedPackages = got.Get(packages);
+            var freshInstalls = new List<Package>();
+            var updateCandidates = new List<Package>();
+
+            // Build a list of all packages that either need to install from scratch,
+            // or those that need to be checked.
+
+            for (int i = 0; i < installedPackages.Length; i++) {
+                var installed = installedPackages[i];
+                if (installed == null) {
+                    freshInstalls.Add(new Package() { Id = packages[i]});
+                } else {
+                    updateCandidates.Add(installed);
+                }
+            }
+
+            // Install all the new packages in paralell to querying the
+            // outdated ones. Install eventual updates last.
+            // Note: Do it serialized in this first version.
+
+            var updates = updateCandidates.ToArray();
+            var currents = (Package[])updates.Clone();
+            var outdatedPackages = installer.QueryLatest(updates);
+
+            foreach (var install in freshInstalls) {
+                var installed = installer.Install(folder, install);
+                got.Save(installed);
+            }
+
+            if (outdatedPackages > 0) {
+                for (int i = 0; i < updates.Length; i++) {
+                    var update = updates[i];
+                    var current = currents[i];
+                    if (update.Version == null || update.Version != current.Version) {
+                        var installed = installer.Install(folder, update);
+                        got.Save(installed);
+                    }
+                }
+            }
+        }
+    }
+}
