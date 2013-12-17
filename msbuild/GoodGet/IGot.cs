9921a0e3967b317a732c6b506dae72b3963b4c08
Per Samuelsson <per@starcounter.com>
2013-12-17 08:57:12 +0100

    Added the IGot interface, exposing the methods used by GoodGet to query what packages we got as well as to store information about new packages retrieved.

---
+++ msbuild/GoodGet/IGot.cs
@@ -0,0 +1,20 @@
+﻿
+namespace GoodGet {
+    /// <summary>
+    /// Expose the packages GoodGet got, including their version.
+    /// </summary>
+    public interface IGot {
+        /// <summary>
+        /// Gets the set of packages specified in <paramref name="packages"/>.
+        /// </summary>
+        /// <param name="packages">The packages to retreive.</param>
+        /// <returns>An array of <see cref="Package"/>s.</returns>
+        public Package[] Get(string[] packages);
+        
+        /// <summary>
+        /// Saves the current package information for later retreival.
+        /// </summary>
+        /// <param name="package">The <see cref="Package"/> to save.</param>
+        public void Save(Package package);
+    }
+}
