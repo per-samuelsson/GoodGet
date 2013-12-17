d57dc7220d1dc20b6edf9c891dd5e6f9849cc909
Per Samuelsson <per@starcounter.com>
2013-12-17 08:55:39 +0100

    Added the Package class to GoodGet, representing a package we've got (including it's version and when we got it).

---
+++ msbuild/GoodGet/Package.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace GoodGet {
+
+    /// <summary>
+    /// Represents a NuGet package GoodGet got.
+    /// </summary>
+    public sealed class Package {
+        /// <summary>
+        /// The package identity.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The package version.
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// The time the package was retreived.
+        /// </summary>
+        public DateTime? Retreived { get; set; }
+    }
+}
