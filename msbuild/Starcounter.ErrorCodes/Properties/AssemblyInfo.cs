a079218b7ffdb72db4a7dd874080e944bb28ac17
Per Samuelsson <per@starcounter.com>
2013-12-17 22:40:22 +0100

    Removed leftovers from the split-up from Starbits.

--- msbuild/Starcounter.ErrorCodes/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Starcounter.ErrorCodes")]
-[assembly: AssemblyDescription("Expose the Starcounter error codes to .NET consumers.")]
-
-[assembly: Guid("388bcdad-55de-4838-bf89-094f05361de1")]
--- src/ErrorCodeCompiler/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("ErrorCodeCompiler")]
-[assembly: AssemblyDescription("Tool that reads errorcodes.xml and spits out code based on it.")]
-
-[assembly: Guid("d67388e3-ff09-40b7-b891-22e65a4b6a3e")]
