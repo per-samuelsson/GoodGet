f037ea4dfad23f471e48ffddecb61a48ea012234
Per Samuelsson <per@starcounter.com>
2013-12-17 09:01:56 +0100

    Added GotNone implementation of IGot, always returning no packages are installed whatsoever.

---
+++ msbuild/GoodGet/GotNone.cs
@@ -0,0 +1,18 @@
+﻿
+namespace GoodGet {
+
+    /// <summary>
+    /// Implements <see cref="IGet"/> always returning no 
+    /// packages are installed whatsoever.
+    /// </summary>
+    internal class GotNone : IGot {
+
+        Package[] IGot.Get(string[] packages) {
+            return new Package[packages.Length];
+        }
+
+        void IGot.Save(Package package) {
+            // Nothing
+        }
+    }
+}
