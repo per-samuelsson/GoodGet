ac0c68e349045435000edc286cff651bcb4eebf8
Per Samuelsson <per@starcounter.com>
2013-12-17 11:37:09 +0100

    Added interfaces IInstallerFactory and IInstaller to GoodGet, exposing the interface to installers used in GoodGet when downloading and installing packages.

---
+++ msbuild/GoodGet/IInstaller/IInstaller.cs
@@ -0,0 +1,17 @@
+﻿
+namespace GoodGet {
+    /// <summary>
+    /// Expose functionality that knows how to install a set of packages
+    /// into a given packages folder.
+    /// </summary>
+    public interface IInstaller {
+        /// <summary>
+        /// Installs all given packages into <see cref="PackagesFolder"/>.
+        /// </summary>
+        /// <param name="folder">The folder to install packages into.</param>
+        /// <param name="packages">The packages to install.</param>
+        /// <param name="got">The authourity to ask when the installer need
+        /// to know what packages are installed.</param>
+        void Install(PackagesFolder folder, FeedPackages packages, IGot got);
+    }
+}
---
+++ msbuild/GoodGet/IInstaller/IInstallerFactory.cs
@@ -0,0 +1,14 @@
+﻿
+namespace GoodGet {
+    /// <summary>
+    /// Factory that knows how to construct <see cref="IInstaller"/>
+    /// instances.
+    /// </summary>
+    public interface IInstallerFactory {
+        /// <summary>
+        /// Creates a new installer.
+        /// </summary>
+        /// <returns>An instance of an <see cref="IInstaller"/>.</returns>
+        IInstaller CreateInstaller();
+    }
+}
