0dfcedb09c92e17f68b457b1676a5b534a3da283
Per Samuelsson <per@starcounter.com>
2013-12-18 10:57:27 +0100

    Added class UpdateAlwaysAuthority, impmplementing IUpdateAuthority by instructing clients to update all packages on every call.

---
+++ src/GoodGet/IUpdateAuthority/UpdateAlwaysAuthority.cs
@@ -0,0 +1,32 @@
+﻿
+namespace GoodGet {
+
+    /// <summary>
+    /// Implements <see cref="IUpdateAuthority"/>, instructing callers
+    /// to update to a latest, unknown version on every call.
+    /// </summary>
+    internal sealed class UpdateAlwaysAuthority : IUpdateAuthority {
+        readonly Feed feed;
+
+        /// <summary>
+        /// Initialize a new <see cref="UpdateAlwaysAuthority"/> instance.
+        /// </summary>
+        /// <param name="feed">The feed to update from.</param>
+        public UpdateAlwaysAuthority(Feed feed) {
+            this.feed = feed;
+        }
+
+        /// <inheritdoc/>
+        Feed IUpdateAuthority.Feed {
+            get { return feed; }
+        }
+
+        /// <inheritdoc/>
+        int IUpdateAuthority.CheckForUpdates(Package[] packages) {
+            foreach (var package in packages) {
+                package.Version = null;
+            }
+            return packages.Length;
+        }
+    }
+}
