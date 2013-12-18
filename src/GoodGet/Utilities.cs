2812a920ab4ba058ee5f99c9c35dfe8ba82ef82b
Per Samuelsson <per@starcounter.com>
2013-12-18 14:10:57 +0100

    Added static class ToolProcess to Utilities, allowing executables to be run as tools in a simple manner.

--- src/GoodGet/Utilities.cs
+++ src/GoodGet/Utilities.cs
@@ -1,8 +1,38 @@
-﻿using System.IO;
+﻿
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace GoodGet {
 
     internal static class Utilities {
+
+        public static class ToolProcess {
+
+            public static int Invoke(ProcessStartInfo processStartInfo, List<string> output = null, List<string> errorOutput = null) {
+                processStartInfo.ErrorDialog = false;
+                processStartInfo.UseShellExecute = false;
+                processStartInfo.CreateNoWindow = true;
+                processStartInfo.RedirectStandardError = true;
+                processStartInfo.RedirectStandardOutput = true;
+                var process = new Process {
+                    StartInfo = processStartInfo
+                };
+                if (output != null) {
+                    process.OutputDataReceived += (sender, e) => { output.Add(e.Data); };
+                }
+                if (errorOutput != null) {
+                    process.ErrorDataReceived += (sender, e) => { errorOutput.Add(e.Data); };
+                }
+
+                process.Start();
+                process.BeginErrorReadLine();
+                process.BeginOutputReadLine();
+                process.WaitForExit();
+
+                return process.ExitCode;
+            }
+        }
 
         public static void AssureDirectory(string path, FileAttributes attributes = FileAttributes.Normal) {
             if (!Directory.Exists(path)) {
