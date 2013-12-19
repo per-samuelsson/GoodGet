d162da98ea594bb1314910cc5a89163345cfe07a
Per Samuelsson <per@starcounter.com>
2013-12-19 14:35:46 +0100

    Added support for custom memberwise cloning of Package instances, and exposed this functionality via IClonable.

--- src/GoodGet/Package.cs
+++ src/GoodGet/Package.cs
@@ -6,7 +6,7 @@ namespace GoodGet {
     /// <summary>
     /// Represents a NuGet package GoodGet got.
     /// </summary>
-    public sealed class Package {
+    public sealed class Package: ICloneable {
         /// <summary>
         /// The package identity.
         /// </summary>
@@ -21,5 +21,19 @@ namespace GoodGet {
         /// The time this package was last installed/updated.
         /// </summary>
         public DateTime? Installed { get; set; }
+
+        /// <summary>
+        /// Clones the current package using a memberwise copy
+        /// strategy, similar to <see cref="object.MemberwiseClone"/>.
+        /// </summary>
+        /// <returns>A clone of the current <see cref="Package"/>.
+        /// </returns>
+        public new Package MemberwiseClone() {
+            return new Package { Id = Id, Version = Version, Installed = Installed };
+        }
+
+        object ICloneable.Clone() {
+            return MemberwiseClone();
+        }
     }
 }
