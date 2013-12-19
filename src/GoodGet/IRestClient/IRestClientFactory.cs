beed8303f1d9390c99be434c9881085f132ff935
Per Samuelsson <per@starcounter.com>
2013-12-19 09:50:30 +0100

    Added interface IRestClientFactory to be able to create IRestClient instances on the fly.

---
+++ src/GoodGet/IRestClient/IRestClientFactory.cs
@@ -0,0 +1,17 @@
+﻿
+namespace GoodGet {
+    /// <summary>
+    /// Represent a factory that knows how to construct
+    /// <see cref="IRestClient"/> instances.
+    /// </summary>
+    public interface IRestClientFactory {
+        /// <summary>
+        /// Creates an instance of a class implementing the
+        /// <see cref="IRestClient"/> interface.
+        /// </summary>
+        /// <param name="feed">The <see cref="Feed"/> the client
+        /// should request it's data from.</param>
+        /// <returns>Instance of a <see cref="IRestClient"/>.</returns>
+        IRestClient CreateClient(Feed feed);
+    }
+}
