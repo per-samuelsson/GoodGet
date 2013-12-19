925819e57e223bcc290b9bc746221c618b501873
Per Samuelsson <per@starcounter.com>
2013-12-19 12:11:41 +0100

    Made first functioning implementation of the GoodGet CLI entrypoint. Still some work to be done to make it support all options.

--- src/GoodGet/Program.cs
+++ src/GoodGet/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Collections.Generic;
 
 namespace GoodGet {
 
@@ -11,7 +12,47 @@ namespace GoodGet {
                 return;
             }
 
-            throw new NotImplementedException();
+            var packagesFolder = string.Empty;
+            var packages = new List<string>();
+            var verbose = false;
+            var quiet = false;
+
+            foreach (var arg in args) {
+                if (arg.StartsWith("-")) {
+                    var option = arg.Substring(1).ToLower();
+                    switch (option) {
+                        case "verbose":
+                            verbose = true;
+                            break;
+                        case "quiet":
+                            quiet = true;
+                            break;
+                        case "got":
+                            Console.WriteLine("Option 'got' is not yet supported");
+                            return;
+                        default:
+                            Console.WriteLine("Option {0} not recognized", option);
+                            Usage();
+                            return;
+                    }
+                } else if (packagesFolder == string.Empty) {
+                    packagesFolder = arg;
+                } else {
+                    packages.Add(arg);
+                }
+            }
+
+            if (packagesFolder == string.Empty) {
+                Console.WriteLine("No packages folder given");
+                Usage();
+                return;
+            } else if (packages.Count == 0) {
+                Console.WriteLine("Specify at least one package");
+                Usage();
+                return;
+            }
+
+            PackagesFolder.InstallPackages(packagesFolder, packages.ToArray());
         }
 
         static void Usage() {
