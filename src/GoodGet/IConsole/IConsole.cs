345dcb3f1e850dec60680377b1f0d70a530db6b7
Per Samuelsson <per@starcounter.com>
2013-12-19 10:55:58 +0100

    Added interface IConsole to GoodGet, to abstract away the user interface.

---
+++ src/GoodGet/IConsole/IConsole.cs
@@ -0,0 +1,24 @@
+﻿
+namespace GoodGet {
+    /// <summary>
+    /// Expose methods to write to a "console", i.e. an output
+    /// stream that normally somehow reach the user.
+    /// </summary>
+    public interface IConsole {
+        /// <summary>
+        /// Writes the output using the severity level considered to
+        /// be most standard to the actual implementation.
+        /// </summary>
+        /// <param name="format">A composite string format string.</param>
+        /// <param name="args">Arguments to insert.</param>
+        void WriteLine(string format, params object[] args);
+
+        /// <summary>
+        /// Writes the output using the severity level specified.
+        /// </summary>
+        /// <param name="severity">The severity of the written content.</param>
+        /// <param name="format">A composite string format string.</param>
+        /// <param name="args">Arguments to insert.</param>
+        void WriteLine(int severity, string format, params object[] args);
+    }
+}
