bf8eb39cf800034c74bac7e498904ad1e1cd1f13
Per Samuelsson <per@starcounter.com>
2013-12-20 14:46:44 +0100

    Added new project form upcoming Continuous Package Versioning algorithm.

---
+++ src/ContinuousPackageVersioning/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+
+namespace ContinuousPackageVersioning {
+    class Program {
+        static void Main(string[] args) {
+            // https://gist.github.com/per-samuelsson/f74e31f3d959b852dccb
+            throw new NotImplementedException();
+        }
+    }
+}
