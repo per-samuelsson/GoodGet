45975c75af61df204fa1849b727a2f4c4da4b55f
Per Samuelsson <per@starcounter.com>
2013-12-20 15:22:01 +0100

    Made the entrypoint run the unit test Test.IsCompatible().

--- src/ContinuousPackageVersioning/Program.cs
+++ src/ContinuousPackageVersioning/Program.cs
@@ -1,10 +1,13 @@
 ﻿
+using ContinuousPackageVersioning.UnitTests;
 using System;
 
 namespace ContinuousPackageVersioning {
+
     class Program {
         static void Main(string[] args) {
             // https://gist.github.com/per-samuelsson/f74e31f3d959b852dccb
+            Test.IsCompatible();
             throw new NotImplementedException();
         }
     }
