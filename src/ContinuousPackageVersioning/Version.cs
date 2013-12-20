6ab7d0d56d27e303be334902bd6737d906032fbc
Per Samuelsson <per@starcounter.com>
2013-12-20 15:48:03 +0100

    Started outlining some of the core CPV versioning statements in the Version class.

--- src/ContinuousPackageVersioning/Version.cs
+++ src/ContinuousPackageVersioning/Version.cs
@@ -2,10 +2,51 @@
 using System;
 
 namespace ContinuousPackageVersioning {
+
     /// <summary>
     /// Expose the core API of the Continuous Package Versioning algorithm.
     /// </summary>
     public sealed class Version {
+        /// <summary>
+        /// The stable part of the CPV/SemVer version.
+        /// </summary>
+        public string Stable { get; private set; }
+        /// <summary>
+        /// The prerelease part of the CPV/SemVer version.
+        /// </summary>
+        public string Prerelease { get; private set; }
+        /// <summary>
+        /// The CPV version.
+        /// </summary>
+        public string CPVVersion { get; private set; }
+
+        private Version(string stable, string prerelease, string cpvVersion) {
+            Stable = stable;
+            Prerelease = prerelease;
+            CPVVersion = cpvVersion;
+        }
+
+        /// <summary>
+        /// Gets the next version of a CPV-compatible version string.
+        /// </summary>
+        /// <param name="specified">The version as specified in its
+        /// 'naked' form, e.g. "1.2.3-alpha".</param>
+        /// <param name="current">The current version, if such exist.</param>
+        /// <returns>The CPV version following the current one.</returns>
+        public static Version GetNext(string specified, string current) {
+            if (string.IsNullOrWhiteSpace(specified) && string.IsNullOrWhiteSpace(current)) {
+                throw new ArgumentNullException("specified", "Specify at least either 'version' or 'current'.");
+            } else if (string.IsNullOrWhiteSpace(current)) {
+                // Just validate specified looks right and apply the
+                // starting number to it.
+                // TODO:
+            }
+
+            // Do our thing
+            // TODO:
+            throw new NotImplementedException();
+        }
+        
         /// <summary>
         /// Gets a result indicating if the given <paramref name="version"/>
         /// is a legal CPV version string.
@@ -52,7 +93,18 @@ namespace ContinuousPackageVersioning {
             prerelease = tokens[1];
         }
 
+        /// <inheritdoc/>
+        public override string ToString() {
+            return Stable + "-" + Prerelease + "." + CPVVersion;
+        }
+
         static bool IsValidStableSemVerVersion(string version) {
+            // Eventually, do this verification too.
+            // TODO:
+            return true;
+        }
+
+        static bool IsValidPrereleaseVersion(string version) {
             // Eventually, do this verification too.
             // TODO:
             return true;
