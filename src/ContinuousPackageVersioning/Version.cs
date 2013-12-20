b1108a1b0a1b70b827457241288bb6354d54b932
Per Samuelsson <per@starcounter.com>
2013-12-20 15:20:27 +0100

    Added the Version class to Continuous Package Versioning project, exposing the core API provided by the same module.

---
+++ src/ContinuousPackageVersioning/Version.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+
+namespace ContinuousPackageVersioning {
+    /// <summary>
+    /// Expose the core API of the Continuous Package Versioning algorithm.
+    /// </summary>
+    public sealed class Version {
+        /// <summary>
+        /// Gets a result indicating if the given <paramref name="version"/>
+        /// is a legal CPV version string.
+        /// </summary>
+        /// <param name="version">The version to evaluate.</param>
+        /// <returns><c>true</c> if compatible; <c>false</c> otherwise.</returns>
+        public static bool IsCompatible(string version) {
+            string ignored1, ignored2;
+            // Poor implementation; swich to non-exception-based eventually
+            try {
+                SplitStableFromPrerelease(version, out ignored1, out ignored2);
+            } catch {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Splits a CPV-compatible string into its stable and it's
+        /// prerelease part.
+        /// </summary>
+        /// <param name="version">The version to split.</param>
+        /// <param name="stable">The stable version.</param>
+        /// <param name="prerelease">The prerelease version.</param>
+        /// <remarks>
+        /// The hyphen in between will not be part of the results.
+        /// </remarks>
+        public static void SplitStableFromPrerelease(string version, out string stable, out string prerelease) {
+            if (string.IsNullOrWhiteSpace(version)) {
+                throw new ArgumentNullException("version");
+            } else if (version.Contains("+")) {
+                throw new ArgumentOutOfRangeException("Build numbers are not supported.");
+            }
+
+            var tokens = version.Split('-');
+            if (tokens.Length != 2 || 
+                string.IsNullOrWhiteSpace(tokens[0]) || 
+                string.IsNullOrWhiteSpace(tokens[1]) ||
+                !IsValidStableSemVerVersion(tokens[0])) {
+                throw new ArgumentOutOfRangeException("SemVer-compatible, prerelease version string expected.");
+            }
+
+            stable = tokens[0];
+            prerelease = tokens[1];
+        }
+
+        static bool IsValidStableSemVerVersion(string version) {
+            // Eventually, do this verification too.
+            // TODO:
+            return true;
+        }
+    }
+}
