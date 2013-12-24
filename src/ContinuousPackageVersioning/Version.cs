eff8c049b3bfe210df224a3412dcca5d4236ab7e
Per Samuelsson <per@starcounter.com>
2013-12-24 12:06:27 +0100

    Finalized a first working, but still a little messy implementation, of Version.GetNext(string).

--- src/ContinuousPackageVersioning/Version.cs
+++ src/ContinuousPackageVersioning/Version.cs
@@ -10,6 +10,7 @@ namespace ContinuousPackageVersioning {
     /// </summary>
     public sealed class Version {
         const string startingVersion = "00000";
+        const int highestCpvNumber = 99999;
         static Regex cpvPrereleaseRegex = new Regex(@"\.(\d{5})\z");
 
         /// <summary>
@@ -38,10 +39,10 @@ namespace ContinuousPackageVersioning {
         /// <param name="version">The version string to parse.</param>
         /// <returns>A <see cref="Version"/> instance.</returns>
         public static Version Parse(string version) {
-            string stable, prerelease;
+            string stable, prerelease, cpv;
             SplitStableFromPrerelease(version, out stable, out prerelease);
-            var tokens = DoRegexSplitOfPrerelease(prerelease);
-            return new Version(stable, tokens[0], tokens[1]);
+            SplitCPVFromPrerelease(prerelease, out prerelease, out cpv);
+            return new Version(stable, prerelease, cpv);
         }
 
         /// <summary>
@@ -52,7 +53,7 @@ namespace ContinuousPackageVersioning {
         /// <param name="current">The current version, if such exist.</param>
         /// <returns>The CPV version following the current one.</returns>
         public static Version GetNext(string specified, string current = null) {
-            string stable, prerelease;
+            string stable, prerelease, cpv;
             if (string.IsNullOrWhiteSpace(specified) && string.IsNullOrWhiteSpace(current)) {
                 throw new ArgumentNullException("specified", "Specify at least either 'version' or 'current'.");
             } else if (string.IsNullOrWhiteSpace(current)) {
@@ -70,22 +71,10 @@ namespace ContinuousPackageVersioning {
                 }
             }
 
-            // Always emanate from the current number. All we needed from
-            // the specified one is now passed.
-            // The valid pattern
-            // [major].[minor].[patch]-[prerelease][.nnnnn]
-
-            // Do our thing
-            // TODO:
-
-            // Here is a regex to see a given current CPV suffix is
-            // actually valid:
-            // var r = new Regex(@"\.(\d{5})\z");
-            // r.IsMatch("per.00001"): // True
-            // r.IsMatch("per.00001a"): // False
-            // r.IsMatch("per00001"): // False
-
-            throw new NotImplementedException();
+            SplitStableFromPrerelease(current, out stable, out prerelease);
+            SplitCPVFromPrerelease(prerelease, out prerelease, out cpv);
+            cpv = ParseAndIncreaseCPVVersionString(cpv);
+            return new Version(stable, prerelease, cpv);
         }
         
         /// <summary>
@@ -140,26 +129,38 @@ namespace ContinuousPackageVersioning {
         }
 
         static bool IsValidStableSemVerVersion(string version) {
-            // Eventually, do this verification too.
+            // Implement this eventually.
             // TODO:
             return true;
         }
 
         static bool IsValidPrereleaseVersion(string prerelease) {
             try {
-                DoRegexSplitOfPrerelease(prerelease);
+                string a, b;
+                SplitCPVFromPrerelease(prerelease, out a, out b);
             } catch {
                 return false;
             }
             return true;
         }
 
-        static string[] DoRegexSplitOfPrerelease(string prerelease) {
-            var tokens = cpvPrereleaseRegex.Split(prerelease);
+        static void SplitCPVFromPrerelease(string prereleaseWithCPV, out string prerelease, out string cpv) {
+            var tokens = cpvPrereleaseRegex.Split(prereleaseWithCPV);
             if (tokens.Length != 3 || tokens[2] != string.Empty) {
-                throw new ArgumentOutOfRangeException("Prerelease part is not CPV compatible: {0}", prerelease);
+                throw new ArgumentOutOfRangeException("Prerelease part is not CPV compatible: {0}", prereleaseWithCPV);
             }
-            return tokens;
+            prerelease = tokens[0];
+            cpv = tokens[1];
+        }
+
+        static string ParseAndIncreaseCPVVersionString(string cpvString) {
+            var cpv = int.Parse(cpvString);
+            if (++cpv > highestCpvNumber) {
+                // Series exhausted.
+                // TODO:
+                throw new ArithmeticException();
+            }
+            return string.Format("{0:D5}", cpv);
         }
     }
 }
